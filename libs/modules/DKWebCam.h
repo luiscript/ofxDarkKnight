2a87f0e7c6a82d9d4a92a4f422864e5bf4a3f919
unknown <isc.luisfer@gmail.com>
2020-05-06 14:50:32 -0500

    Basic WebCam Input Module

--- libs/modules/DKWebCam.h
+++ libs/modules/DKWebCam.h
@@ -1,16 +1,16 @@
 /*
- Copyright (C) 2019 Luis Fernando GarcÃ­a PÃ©rez [http://luiscript.com]
- 
+ Copyright (C) 2020 Luis Fernando García Pérez [http://luiscript.com]
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,14 +20,25 @@
  SOFTWARE.
  */
 
-#include "DKChain.h"
-#include "DKConfig.hpp"
-#include "DKLight.hpp"
-#include "DKLiveShader.hpp"
-#include "DKLfo.hpp"
-#include "DKPreview.hpp"
-#include "DKMixer.hpp"
-#include "DKPerlin.hpp"
-#include "DKScreenOutput.hpp"
-#include "DKSliderInverter.hpp"
-#include "DKWebCam.h"
+
+#include "DKModule.hpp"
+
+class DKWebCam : public DKModule
+{
+private:
+	float camWidth;
+	float camHeight;
+	ofFbo* fbo;
+	bool drawFbo = false;
+	float scaleX;
+	float scaleY;
+
+	ofVideoGrabber vidGrabber;
+public:
+
+	void setup();
+	void update();
+	void draw();
+	void addModuleParameters();
+	ofFbo* getFbo();
+};
